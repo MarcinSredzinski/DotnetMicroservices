ab4dd03a0eac99ec10ca140deb83f5e3f2339872
m.sredzinski94@gmail.com <m.sredzinski94@gmail.com>
2023-07-03 23:16:49 +0200

    Added action to get platform by id

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -5,24 +5,40 @@ using PlatformService.DTOs;
 
 namespace PlatformService.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/[controller]/[action]")]
 [ApiController]
 public class PlatformsController : Controller
 {
     private readonly IPlatformRepository _repository;
     private readonly IMapper _mapper;
-    private readonly ILogger _logger;
+    private readonly ILogger<PlatformsController> _logger;
 
-    public PlatformsController(IPlatformRepository repo, IMapper mapper, ILogger logger)
+    public PlatformsController(IPlatformRepository repo, IMapper mapper, ILogger<PlatformsController> logger)
     {
         _repository = repo;
         _mapper = mapper;
         _logger = logger;
     }
+
+    [HttpGet]
     public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
     {
         _logger.LogInformation("--> Getting Platforms");
 
         return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(_repository.GetAll()));
     }
+
+    [Route("{id}")]
+    [HttpGet]
+    public ActionResult<PlatformReadDto> GetPlatformById(int id)
+    {
+        _logger.LogInformation("--> Getting Platforms");
+        var platform = _mapper.Map<PlatformReadDto>(_repository.Get(id));
+        if (platform is null)
+        {
+            return NotFound();
+        }
+        return Ok(platform);
+    }
+
 }
