0a577666a99e9710f1f354b2d573c068d509c80d
m.sredzinski94@gmail.com <m.sredzinski94@gmail.com>
2023-08-11 08:24:36 +0200

    Added platforms controller to commands service for later use

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers;
+
+[Route("api/c/[controller]")]
+[ApiController]
+public class PlatformsController : Controller
+{
+    [HttpPost]
+    public IActionResult TestInboundConnection()
+    {
+        Console.WriteLine("--> Inpbound POST #Command Service");
+        return Ok("Inbound test of Platforms Controller in Commands Service");
+    }
+}
--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -2,22 +2,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+//builder.Services.AddScoped<ApiKeyAuthFilter>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
