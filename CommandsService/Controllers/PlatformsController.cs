0869f5986e05b948b536b1c23aba4a7812eca5d3
m.sredzinski94@gmail.com <m.sredzinski94@gmail.com>
2023-08-12 19:43:52 +0200

    Posting data to platforms service WiP

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -6,10 +6,25 @@ namespace CommandsService.Controllers;
 [ApiController]
 public class PlatformsController : Controller
 {
+    private readonly ILogger<PlatformsController> _logger;
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public PlatformsController(ILogger<PlatformsController> logger, IHttpClientFactory httpClientFactory)
+    {
+        _logger = logger;
+        _httpClientFactory = httpClientFactory;
+    }
+
     [HttpPost]
     public IActionResult TestInboundConnection()
     {
-        Console.WriteLine("--> Inpbound POST #Command Service");
-        return Ok("Inbound test of Platforms Controller in Commands Service");
+        using var client = _httpClientFactory.CreateClient("PlatformsHttpClient");
+
+        var response = await client.PostAsJsonAsync($"platforms/CreatePlatform", platform);
+
+        var result = response.Content;
+        var responseBody = await result.ReadFromJsonAsync<PlatformReadDto>();
+
+        return CreatedAtRoute(nameof(GetById), new { responseBody?.Id }, responseBody);
     }
 }
